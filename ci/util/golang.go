4b2782112b53f8724236687b3bb8e973e66ce23b
Dinesh Majrekar <dinesh@civo.com>
2024-10-24 21:15:20 +0100

    Replace go deps dyanmically

--- ci/util/golang.go
+++ ci/util/golang.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"dagger/ci/internal/dagger"
 	"fmt"
+	"strings"
 
 	"golang.org/x/mod/modfile"
 )
@@ -28,14 +29,43 @@ func Run(ctx context.Context, dag *dagger.Client, dir string, rootDir *dagger.Di
 
 	// Set the base container version from the go version in the go.mod file
 	baseContainerVersion := fmt.Sprintf("golang:%s", mod.Go.Version)
+	baseContainer := dag.Container().From(baseContainerVersion)
 
-	return dag.Container().
-		From(baseContainerVersion).
+	replacedMods, err := replacedDepsFromGoMod(goModFile)
+	if err != nil {
+		return "", err
+	}
+	for _, r := range replacedMods {
+		// Mount the replaced dependencies
+		mountPath := strings.Replace(r, "../", "", 1)
+		baseContainer = baseContainer.WithDirectory("/go/src/github.com/dmajrekar/dagger-cache-monorepo/"+mountPath, rootDir.Directory(mountPath))
+	}
+
+	return baseContainer.
 		WithMountedCache("/go/pkg/mod", cacheVolume, dagger.ContainerWithMountedCacheOpts{Sharing: dagger.Shared}).
 		WithMountedCache("/root/.cache/go-build", cacheBuild).
 		WithDirectory("/go/src/github.com/dmajrekar/dagger-cache-monorepo/"+dir, rootDir.Directory(dir)).
-		WithDirectory("/go/src/github.com/dmajrekar/dagger-cache-monorepo/lib", rootDir.Directory("lib")).
 		WithWorkdir("/go/src/github.com/dmajrekar/dagger-cache-monorepo/" + dir).
 		WithExec([]string{"go", "run", "main.go"}).
 		Stdout(ctx)
+}
+
+// replacedDepsFromGoMod takes a go.mod file and returns the list of
+// replaced dependencies
+func replacedDepsFromGoMod(goModFile string) ([]string, error) {
+	mod, err := modfile.Parse("", []byte(goModFile), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var deps []string
+	for _, r := range mod.Replace {
+		// exit if the replace is not a local path
+		if r.New.Version != "" {
+			continue
+		}
+		deps = append(deps, r.New.Path)
+	}
+
+	return deps, nil
 }
